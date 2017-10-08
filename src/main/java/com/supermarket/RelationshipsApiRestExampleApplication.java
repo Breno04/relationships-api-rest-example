6fd257cf13c3b5b0ab2f08053a935f598e441b86
Breno Castelo Branco <brenocastelo10@hotmail.com>
2017-10-07 22:58:23 -0300

    subindo projeto

---
+++ src/main/java/com/supermarket/RelationshipsApiRestExampleApplication.java
@@ -0,0 +1,12 @@
+package com.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationshipsApiRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationshipsApiRestExampleApplication.class, args);
+	}
+}
